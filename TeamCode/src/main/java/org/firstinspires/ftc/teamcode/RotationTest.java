1d1a0fe0ae3210446180f22ac0ef0880f3cd6a4f
rapidpadre <henryrdurso@icloud.com>
2025-10-03 12:34:01 -0700

    rotation works?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotationTest.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "RotationTest")
+public class RotationTest extends OpMode {
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+
+    @Override
+    public void init() {
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            leftDrive.setPower(1);
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            leftDrive.setPower(0);
+        }
+        else if (gamepad1.b) {
+            rightDrive.setPower(1);
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            rightDrive.setPower(0);
+        }
+
+        telemetry.addData("Left Encoder", leftDrive.getCurrentPosition());
+        telemetry.addData("Right Encoder", rightDrive.getCurrentPosition());
+        telemetry.update();
+    }
+}
