4f8515724b9b91caad5ac17758e7b2dd85338a6e
Beebop413 <bianca.m.lamb.413@gmail.com>
2025-10-18 09:06:43 -0700

    Update LaunchTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp (name = "LaunchTest")
 public class LaunchTest extends OpMode {
@@ -11,12 +12,14 @@ public class LaunchTest extends OpMode {
     private DcMotor potatoCannonTwo = null;
     private int motor1Direction = 1;
     private int motor2Direction = 0 * motor1Direction * -1;
+    private Servo actuator = null;
 
     @Override
     public void init() {
 
         potatoCannon = hardwareMap.get(DcMotor.class, "flywheel");
         potatoCannonTwo = hardwareMap.get(DcMotor.class, "flywheelTwo");
+        actuator = hardwareMap.get(Servo.class, "gate");
 
     }
 
@@ -42,6 +45,13 @@ public class LaunchTest extends OpMode {
         { //kill power
             potatoCannon.setPower(0 * motor1Direction);
             potatoCannonTwo.setPower(0 * motor2Direction);
+        }
+        else if (gamepad1.dpad_up) {
+            actuator.setPosition(0.2);
+        }
+        else if (gamepad1.dpad_down) {
+            actuator.setPosition(0.75);
+
         }
 
     }
