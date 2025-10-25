6048430b0eba3ca8ab03e402a185bb944586d8a9
oliver-error <oliverliamandersen2011@gmail.com>
2025-10-25 12:37:11 -0700

    ADDED A MODIFIED AUTO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFar.java
@@ -18,11 +18,18 @@ public class AutonomousFar extends LinearOpMode {
         gate = hardwareMap.get(Servo.class, "gate");
 
         flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        double power = 0.65;
 
         gate.setPosition(0.1);
-
+        while(!(gamepad1.a || gamepad1.b))
+        {
+            Thread.sleep(50);
+        }
+        if(gamepad1.a) {
+            power = 0.55;
+        }
         waitForStart();
-        flywheel.setPower(0.65);
+        flywheel.setPower(power);
         Thread.sleep(7000);
         gate.setPosition(0.65);
         Thread.sleep(500);
