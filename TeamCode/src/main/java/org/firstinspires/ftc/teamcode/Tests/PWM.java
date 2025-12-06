238765023a5f241c1d961eed2381139ce2ae2751
oliver-error <oliverliamandersen2011@gmail.com>
2025-12-06 12:12:00 -0800

    PWM game in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/PWM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/PWM.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.Tests;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -15,6 +14,20 @@ public class PWM extends OpMode
     private double green = 0;
     private double purple = 0.71;
     private double delete = 0.3;
+    private int ledDis = 0;
+    private double num = 0;
+
+    private double led1col = 0.3;
+    private double led2col = 0.3;
+    private double led3col = 0.3;
+    private boolean led1Done = false;
+    private boolean led2Done = false;
+    private boolean led3Done = false;
+    private double diff12 = 0;
+    private double diff13 = 0;
+    private double diff23 = 0;
+    private boolean isWIn = false;
+
 
     @Override
     public void init() {
@@ -31,7 +44,7 @@ public class PWM extends OpMode
                 led2.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
                 led3.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
         }
-        if(gamepad1.y){
+        if(gamepad1.x){
             //scroll
                 led1.setPosition(0.5);
                 led2.setPosition(0.8);
@@ -60,7 +73,7 @@ public class PWM extends OpMode
 
         }
         if(gamepad1.b) {
-            //slow increase
+            //RGB lights
                 led1.setPosition(delete);
                 led2.setPosition(delete);
                 led3.setPosition(delete);
@@ -84,6 +97,68 @@ public class PWM extends OpMode
                     throw new RuntimeException(e);
                 }
         }
+        if(gamepad1.y){
+            while(true){
+                //RGB game
+                telemetry.addData("A?:", gamepad1.a);
+                telemetry.addData("B?:", gamepad1.b);
+                telemetry.addData("X?:", gamepad1.x);
+                telemetry.update();
 
+//                if (delete >= 0.7) num = -0.01;
+//                if (delete <= 0.3) num = 0.01;
+                if (!led1Done) led1.setPosition(led1col);
+                if (led1Done && !led2Done) led2.setPosition(led3col);
+                if (!led3Done && led2Done) led2.setPosition(led2col);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if (led1col >= 0.3) led1col += 0.01;
+                if (led2col >= 0.3) led2col += 0.01;
+                if (led3col >= 0.3) led3col += 0.01;
+
+                if (led1col >= 0.7) led1col = 0.3;
+                if (led2col >= 0.7) led2col = 0.3;
+                if (led3col >= 0.7) led3col = 0.3;
+
+                if (gamepad1.x) led1Done = true;
+                if (gamepad1.a) led2Done = true;
+                if (gamepad1.b) led3Done = true;
+
+                if (led1Done && led2Done && led3Done)
+                {
+                    diff12 = Math.abs(led1col - led2col);
+                    diff13 = Math.abs(led1col - led3col);
+                    diff23 = Math.abs(led2col - led3col);
+                }
+                if (diff23 != 0)
+                {
+                    
+                }
+
+
+
+//                if(!(ledDis >= 1)) led1.setPosition(delete);
+//                if(!(ledDis >= 2)) led2.setPosition(delete);
+//                if(!(ledDis >= 3)) led3.setPosition(delete);
+//                delete += num;
+//                if (delete >= 0.7) num = -0.01;
+//                if (delete <= 0.3) num = 0.01;
+//                try {
+//                    Thread.sleep(50);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                if(gamepad1.x && !(ledDis > 0)) ledDis = 1;
+//                if(gamepad1.a && ledDis == 1) ledDis = 2;
+//                if(gamepad1.b && ledDis == 2) ledDis = 3;
+            }
+        }
+        telemetry.addData("A?:", gamepad1.a);
+        telemetry.addData("B?:", gamepad1.b);
+        telemetry.addData("X?:", gamepad1.x);
+        telemetry.update();
     }
 }
