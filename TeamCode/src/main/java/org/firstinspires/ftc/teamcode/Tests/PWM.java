836ae7ebb38efc089bebc39929fd9e12ebae51f6
oliver-error <oliverliamandersen2011@gmail.com>
2025-12-06 09:28:56 -0800

    made danceparty more bertterer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/PWM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/PWM.java
@@ -25,28 +25,65 @@ public class PWM extends OpMode
 
     @Override
     public void loop() {
+        if(gamepad1.a) {
+            while(true) {
+                led1.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
+                led2.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
+                led3.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
+            }
+        }
+        if(gamepad1.y){
+            //scroll
+            while(true){
+                led1.setPosition(0.5);
+                led2.setPosition(0.8);
+                led3.setPosition(0.8);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                led1.setPosition(0.8);
+                led2.setPosition(0.5);
+                led3.setPosition(0.8);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                led1.setPosition(0.8);
+                led2.setPosition(0.8);
+                led3.setPosition(0.5);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+        if(gamepad1.b) {
+            while(true) {
+                led1.setPosition(delete);
+                led2.setPosition(delete);
+                led3.setPosition(delete);
+                delete += 0.01;
+                if (delete > 0.7) delete = 0;
+                if (delete < 0.3) delete = 0.3;
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                led1.setPosition(purple);
+                led2.setPosition(purple);
+                led3.setPosition(purple);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 
-//        led1.setPosition(delete);
-//        led2.setPosition(delete);
-//        led3.setPosition(delete);
-//               delete += 0.01;
-//        if(delete > 0.7) delete = 0;
-//        if(delete < 0.3) delete = 0.3;
-//        try {
-//            Thread.sleep(50);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        led1.setPosition(purple);
-//        led2.setPosition(purple);
-//        led3.setPosition(purple);
-//        try {
-//            Thread.sleep(50);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-        led1.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
-        led2.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
-        led3.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
     }
 }
