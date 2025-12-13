64dc34e33d3319063820a6b0780c7f4f431a8c47
oliver-error <oliverliamandersen2011@gmail.com>
2025-12-13 10:40:08 -0800

    made gambling betterer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/PWM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/PWM.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "'aw dang it'")
+@TeleOp(name = "Lets go gambling")
 public class PWM extends OpMode
 {
 
@@ -27,7 +27,6 @@ public class PWM extends OpMode
     private double diff13 = 0;
     private double diff23 = 0;
     private boolean isWIn = false;
-    private double rigVal = 0.2;
 
 
     @Override
@@ -39,234 +38,158 @@ public class PWM extends OpMode
 
     @Override
     public void loop() {
-//        if(gamepad1.a) {
-//            //random
-//                led1.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
-//                led2.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
-//                led3.setPosition(0.1 + (Math.random() * (0.8 - 0.1)));
-//        }
-//        if(gamepad1.x){
-//            //scroll
-//                led1.setPosition(0.5);
-//                led2.setPosition(0.8);
-//                led3.setPosition(0.8);
-//                try {
-//                    Thread.sleep(500);
-//                } catch (InterruptedException e) {
-//                    Thread.currentThread().interrupt();
-//                }
-//                led1.setPosition(0.8);
-//                led2.setPosition(0.5);
-//                led3.setPosition(0.8);
-//                try {
-//                    Thread.sleep(500);
-//                } catch (InterruptedException e) {
-//                    Thread.currentThread().interrupt();
-//                }
-//                led1.setPosition(0.8);
-//                led2.setPosition(0.8);
-//                led3.setPosition(0.5);
-//                try {
-//                    Thread.sleep(500);
-//                } catch (InterruptedException e) {
-//                    Thread.currentThread().interrupt();
-//                }
-//
-//        }
-//        if(gamepad1.b) {
-//            //RGB lights
-//                led1.setPosition(delete);
-//                led2.setPosition(delete);
-//                led3.setPosition(delete);
-//                delete += 0.01;
-//                if (delete >= 0.7) {
-//                    while(delete >= 0.3){
-//                        led1.setPosition(delete);
-//                        led2.setPosition(delete);
-//                        led3.setPosition(delete);
-//                        delete += -0.01;
-//                        try {
-//                            Thread.sleep(50);
-//                        } catch (InterruptedException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//                    }
-//                }
-//                try {
-//                    Thread.sleep(50);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//        }
-//        if(gamepad1.y){
-//            while(true){
-                //RGB game
-                telemetry.addData("A?", gamepad1.a);
-                telemetry.addData("B?", gamepad1.b);
-                telemetry.addData("X?", gamepad1.x);
-                telemetry.addData("led1", led1col);
-                telemetry.addData("led2", led2col);
-                telemetry.addData("led3", led3col);
-                telemetry.update();
+
+        double rigVal = (((gamepad2.touchpad_finger_1_x) + 1) / 2) + 0.01; // less val = more rigged
+        int speed = (int) Math.abs(Math.floor(((gamepad2.touchpad_finger_1_y) + 1) * 50)); // less val = more fast
+        if(gamepad2.x) rigVal = 0.5; speed = 50;
+        if(gamepad2.a) rigVal = 0.1; speed = 1;
+        if(gamepad2.b) rigVal = 1; speed = 70;
+
+        //RGB game
+        telemetry.addData("led1", led1col);
+        telemetry.addData("led2", led2col);
+        telemetry.addData("led3", led3col);
+        telemetry.addData("speed", speed);
+        telemetry.addData("rig value", rigVal);
+        telemetry.update();
 
 //                if (delete >= 0.7) num = -0.01;
 //                if (delete <= 0.3) num = 0.01;
-                led1.setPosition(led1col);
-                led3.setPosition(led3col);
-                led2.setPosition(led2col);
+        led1.setPosition(led1col);
+        led3.setPosition(led3col);
+        led2.setPosition(led2col);
+        try {
+            Thread.sleep(speed);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (!led1Done) led1col += num;
+        if (!led2Done) led2col += num;
+        if (!led3Done) led3col += num;
+
+        if (led1col >= 0.7 || led2col >= 0.7 || led3col >= 0.7) num = -0.01;
+        if (led1col <= 0.3 || led2col <= 0.3 || led3col <= 0.3) num = 0.01;
+
+        if (gamepad1.x) led1Done = true;
+        if (gamepad1.a) led2Done = true;
+        if (gamepad1.b) led3Done = true;
+
+        if (led1Done && led2Done && led3Done)
+        { //math
+            diff12 = Math.abs(led1col - led2col);
+            diff13 = Math.abs(led1col - led3col);
+            diff23 = Math.abs(led2col - led3col);
+            //fake loading animation
+            led1.setPosition(0);
+            led3.setPosition(0);
+            led2.setPosition(0);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            led1.setPosition(1);
+            led3.setPosition(0);
+            led2.setPosition(0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            led1.setPosition(0);
+            led3.setPosition(1);
+            led2.setPosition(0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            led1.setPosition(0);
+            led3.setPosition(0);
+            led2.setPosition(1);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (diff23 != 0 && diff12 + diff13 + diff23 < rigVal)
+            { // winning animation
+                led1.setPosition(0.5);
+                led3.setPosition(0.5);
+                led2.setPosition(0.5);
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                if (!led1Done) led1col += num;
-                if (!led2Done) led2col += num;
-                if (!led3Done) led3col += num;
-
-                if (led1col >= 0.7 || led2col >= 0.7 || led3col >= 0.7) num = -0.01;
-                if (led1col <= 0.3 || led2col <= 0.3 || led3col <= 0.3) num = 0.01;
-
-                if (gamepad1.x) led1Done = true;
-                if (gamepad1.a) led2Done = true;
-                if (gamepad1.b) led3Done = true;
-
-                if (led1Done && led2Done && led3Done)
-                { //math
-                    diff12 = Math.abs(led1col - led2col);
-                    diff13 = Math.abs(led1col - led3col);
-                    diff23 = Math.abs(led2col - led3col);
-                    //fake loading animation
-                    led1.setPosition(0);
-                    led3.setPosition(0);
-                    led2.setPosition(0);
-                    try {
-                        Thread.sleep(300);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    led1.setPosition(1);
-                    led3.setPosition(0);
-                    led2.setPosition(0);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    led1.setPosition(0);
-                    led3.setPosition(1);
-                    led2.setPosition(0);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    led1.setPosition(0);
-                    led3.setPosition(0);
-                    led2.setPosition(1);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    if (diff23 != 0 && diff12 + diff13 + diff23 < rigVal)
-                    { // winning animation
-                        led1.setPosition(0.5);
-                        led3.setPosition(0.5);
-                        led2.setPosition(0.5);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        led1.setPosition(0);
-                        led3.setPosition(0);
-                        led2.setPosition(0);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        led1.setPosition(0.5);
-                        led3.setPosition(0.5);
-                        led2.setPosition(0.5);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        led1Done = false;
-                        led2Done = false;
-                        led3Done = false;
-                        led1col = 0.3;
-                        led2col = led1col;
-                        led3col = led1col;
-                        try {
-                            Thread.sleep(3000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    else if(!(diff23 != 0 && diff12 + diff13 + diff23 < 0.1))
-                    { //losing animation
-                        led1.setPosition(0.3);
-                        led3.setPosition(0.3);
-                        led2.setPosition(0.3);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        led1.setPosition(0);
-                        led3.setPosition(0);
-                        led2.setPosition(0);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        led1.setPosition(0.3);
-                        led3.setPosition(0.3);
-                        led2.setPosition(0.3);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        led1Done = false;
-                        led2Done = false;
-                        led3Done = false;
-                        led1col = 0.3;
-                        led2col = led1col;
-                        led3col = led1col;
-                        try {
-                            Thread.sleep(3000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-//                    }
-//                }
-
-
-
-
-
-//                if(!(ledDis >= 1)) led1.setPosition(delete);
-//                if(!(ledDis >= 2)) led2.setPosition(delete);
-//                if(!(ledDis >= 3)) led3.setPosition(delete);
-//                delete += num;
-//                if (delete >= 0.7) num = -0.01;
-//                if (delete <= 0.3) num = 0.01;
-//                try {
-//                    Thread.sleep(50);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                if(gamepad1.x && !(ledDis > 0)) ledDis = 1;
-//                if(gamepad1.a && ledDis == 1) ledDis = 2;
-//                if(gamepad1.b && ledDis == 2) ledDis = 3;
-            }
+                led1.setPosition(0);
+                led3.setPosition(0);
+                led2.setPosition(0);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
+                led1.setPosition(0.5);
+                led3.setPosition(0.5);
+                led2.setPosition(0.5);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                led1Done = false;
+                led2Done = false;
+                led3Done = false;
+                led1col = 0.3;
+                led2col = led1col;
+                led3col = led1col;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else if(!(diff23 != 0 && diff12 + diff13 + diff23 < rigVal))
+            { //losing animation
+                led1.setPosition(0.3);
+                led3.setPosition(0.3);
+                led2.setPosition(0.3);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                led1.setPosition(0);
+                led3.setPosition(0);
+                led2.setPosition(0);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                led1.setPosition(0.3);
+                led3.setPosition(0.3);
+                led2.setPosition(0.3);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                led1Done = false;
+                led2Done = false;
+                led3Done = false;
+                led1col = 0.3;
+                led2col = led1col;
+                led3col = led1col;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
         telemetry.update();
     }
 }
